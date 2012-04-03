d4f544b3afbc2c26195a6b7b789168fa51fb9227
Divya Kamania <kamaniad@my.erau.edu>
2012-04-03 09:45:34 -0500

    <interface layouts>

--- src/team/pfm/com/TeamPFMActivity.java
+++ src/team/pfm/com/TeamPFMActivity.java
@@ -8,6 +8,6 @@ public class TeamPFMActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainactivity);
+        setContentView(R.layout.mainpage);
     }
 }
