84f37a98f2c342b25fab5b590a0a5eee54598c5b
Andrew Colton <coltona@my.erau.edu>
2012-04-08 11:26:11 -0400

    got the settings page to work!

---
+++ src/team/pfm/com/SettingsActivity.java
@@ -0,0 +1,16 @@
+package team.pfm.com;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class SettingsActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call the superclass version
+        setContentView(R.layout.settingspage); // set the layout
+	}
+}
